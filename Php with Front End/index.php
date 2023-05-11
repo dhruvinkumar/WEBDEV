fddd949b0d79101168e64437f88b14f7ba976094
DHRUVINKUMAR BHATT <75667686+dhruvinkumar@users.noreply.github.com>
2023-05-11 05:52:59 +0530

    Create index.php

---
+++ Php with Front End/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Web Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<?php include 'header.html'; ?>
+	
+	<?php include 'navigation.html'; ?>
+	
+	<div class="content">
+		<?php include 'content.html'; ?>
+	</div>
+	
+	<?php include 'footer.html'; ?>
+</body>
+</html>
